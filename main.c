f445541bb796697b430c753495bb3cd87e2ee17d
GoulartG16 <geovanagoulart776@gmail.com>
2025-10-16 16:57:38 -0300

    Criar a struct RegistroDoMercado

--- main.c
+++ main.c
@@ -4,6 +4,51 @@
 
 #define true 1
 #define false 0
+//Estoque do Mercado
+typedef enum{COMIDA, PAPELARIA, LIMPEZA, ACOUGUE, HORTIFRUTI}TipoDoProduto;
+
+typedef struct{
+    char marca[20];
+    char tipo[20]; // Arroz, feijão, macarrao
+    char categoria[20]; //seco, perecivel, nao perecivel
+}Comida;
+
+typedef struct{
+    classe[20]; //fruta, legume, folhosa, raiz
+    char variedade[20]; // banana prate, tomate italiano, ...  
+}Hortifruti;
+
+typedef struct{
+    char marca[20];
+    char tipo[20];// caneta, caderno, pincel, ...
+    char detalhe[20];// azul, 200f, 75g, ...
+}Papelaria;
+
+typedef struct{
+    char marca[20];
+    char forma[12]; // liquido, spray, po
+}Limpeza;
+
+typedef struct{
+    char origem[20];// boi, galinha, porco, peixe, ...
+    char corte[20]; //alcatra, peito, lombo, ...
+}Acougue;
+
+typedef struct{
+    int id;
+    float preco;
+    char nome[25];
+    int quantidade;
+    char datadevalidade[11];
+    TipoDoProduto tag;
+    union{
+        Comida comida;
+        Papelaria papelaria;
+        Limpeza limpeza;
+        Acougue acougue;
+        Hortifruti hortifruti;
+    }detalhes;
+}RegistroDoMercado;
 
 void registrar(void);
 void consultar(void);
